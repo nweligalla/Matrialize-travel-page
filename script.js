dc08797746b20f9b76b4862e9251396109d306d1
nweligalla <nayanaweligalla@gmail.com>
2021-01-17 12:49:07 +0530

    added navbar, slider,and search

---
+++ script.js
@@ -0,0 +1,28 @@
+// sideNav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// slider
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+    indicators: false,
+    height: 400,
+    transition: 500,
+    interval: 3000,
+});
+
+// search autocomplete
+
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+        "sigiriya": null,
+        "anuradhapura": null,
+        "dambulla": null,
+        "colombo": null,
+        "jaffna": null,
+        "hikkaduwa": null,
+        "ambuluwawa":null,
+        "kalkuda":null,
+    }
+});
